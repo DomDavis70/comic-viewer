13b79becbac7f5387edce9c53b1caa61af8a6214
domdavis70 <domdavis70@gmail.com>
2024-05-26 12:40:40 -0500

    added basic backend

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import cors from 'cors'; // Import the CORS middleware
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const COMICVINE_API_KEY = process.env.COMICVINE_API_KEY;
+
+// Use the CORS middleware
+app.use(cors());
+
+// app.get('/api/volumes', async (req, res) => {
+//   try {
+//     const response = await fetch(`https://comicvine.gamespot.com/api/volumes/?api_key=${COMICVINE_API_KEY}&format=json&sort=name:asc&limit=10`);
+//     const data = await response.json();
+//     res.json(data);
+//   } catch (error) {
+//     console.error('Error fetching data from Comic Vine API:', error);
+//     res.status(500).json({ error: 'Failed to fetch data' });
+//   }
+// });
+
+app.get('/', (req, res) => {
+  res.send('Backend server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
