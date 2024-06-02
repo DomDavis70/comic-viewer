2e09d6b22b028fa78c3f34bb7b1fcc0889eee1b4
domdavis70 <domdavis70@gmail.com>
2024-06-02 16:28:09 -0500

    adding errors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,13 +9,21 @@ const PORT = process.env.PORT || 8000;
 const COMICVINE_API_KEY = process.env.COMICVINE_API_KEY;
 
 // Use the CORS middleware
-app.use(cors({
-  origin: 'https://www.comic-viewer.com'
-}));
+const corsOptions = {
+  origin: 'https://www.comic-viewer.com', // Replace with your frontend domain
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 app.get('/api/volumes', async (req, res) => {
   try {
     const response = await fetch(`https://comicvine.gamespot.com/api/volumes/?api_key=${COMICVINE_API_KEY}&format=json&sort=name:asc&limit=10`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
     const data = await response.json();
     res.json(data);
   } catch (error) {
@@ -23,6 +31,7 @@ app.get('/api/volumes', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch data' });
   }
 });
+
 
 app.get('/', (req, res) => {
   res.send('Backend server is running!');
